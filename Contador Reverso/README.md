7cc67873f8827c4e3132bb44e637fe05c1e8359c
LucasCelli <lucascelli@live.com>
2022-09-03 11:21:22 -0400

    remover duplicata

--- Contador Reverso/README.md
+++
@@ -1,2 +0,0 @@
-# Contador Reverso
-Um contador que começa em um determinado número e finaliza ao chegar a zero.
--- Contador Reverso/scripts/main.js
+++
@@ -1,8 +0,0 @@
-// Javascript
-let counter = document.querySelector('.counter');
-let count = 20;
-
-let counterInterval = setInterval(() => {
-    count <= 1 && clearInterval(counterInterval);
-    counter.textContent = count <= 10 ? `0${--count}` : `${--count}`;
-}, 1000);
